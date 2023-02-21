0418854f92e0dd7080f1921d34ce068ee0554fe4
kw-andy <andy.kwok.work@gmail.com>
2023-02-21 11:42:48 +0100

    correction des fichiers et mise à nouveau sous Flake 8

--- Bonus.py
+++ Bonus.py
@@ -1,5 +1,4 @@
 import pygame
-from pygame.locals import *
 import random
 
 
@@ -7,16 +6,22 @@ WIDTH = 800
 HEIGHT = 600
 
 # Fonction permettant de créer des bonus à  un emplacement aléatoire
+
+
 def create_bonus():
     bonus = Bonus(random.randint(20, WIDTH * 0.9), random.randint(20, HEIGHT * 0.9), 10)
     return bonus
 
 # Class des bonus
+
+
 class Bonus(pygame.sprite.Sprite):
     def __init__(self, x, y, points):
         super(Bonus, self).__init__()
         self.image = pygame.image.load("Pictures/Bonus/blue_bonus.png")
         self.rect = self.image.get_rect()
-        self.rect.x = x #- self.rect.centerx
-        self.rect.y = y #- self.rect.centery
+        self.rect.x = x
+        # self.rect.centerx
+        self.rect.y = y
+        # self.rect.centery
         self.points = points
